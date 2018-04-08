e68dc83fcd6858c16c37f992818328f22d2969eb
dbakti7 <dbakti1605@gmail.com>
2018-04-08 20:47:18 +0800

    Documentation.

--- src/constants.js
+++ src/constants.js
@@ -5,28 +5,42 @@ function define(name, value) {
   });
 }
 
-// set to true to deploy with HTTPS
+// -------------------- CONFIG --------------------
+
+// set to true to deploy with HTTPS, set to false for local deployment (http)
 define("IS_PRODUCTION", false);
 
-// SSL Certs locations, only used when IS_PRODUCTION is true
-define("CERT_FILE", "/etc/letsencrypt/live/www.pieceofcode.org/fullchain.pem");
-define("KEY_FILE", "/etc/letsencrypt/live/www.pieceofcode.org/privkey.pem");
-
+// set to true to query Dialogflow agent directly
+// set to false to redirect query to server
 define("DIRECT_QUERY", true);
-define("DIALOGFLOW_TOKEN", "58be6f8f4fb9447693edd36fb975bece");
 
-define("LOCALHOST", "http://localhost");
-define("DEPLOYMENT_URL", "https://www.pieceofcode.org");
-define("LOCALHOST_PORT", "3000");
+// used when DIRECT_QUERY = true
+define("DIALOGFLOW_TOKEN", "58be6f8f4fb9447693edd36fb975bece"); 
 
 // set to true to enable client-side spellchecking, false otherwise
 define("USE_SPELLCHECKING", true);
 
-// client-side spellchecking endpoint
-define("PREPROCESS_ENDPOINT", "/preprocess");
+// SSL certs location, only used when IS_PRODUCTION = true
+define("CERT_FILE", "/etc/letsencrypt/live/www.pieceofcode.org/fullchain.pem");
+define("KEY_FILE", "/etc/letsencrypt/live/www.pieceofcode.org/privkey.pem");
 
-// server url and end-point as proxy to Dialogflow
-define("SERVER_URL", "https://www.pieceofcode.org:8080");
-define("SERVER_URL_LOCAL", "http://localhost:8080");
-define("SERVER_ENDPOINT", "/internal-query");
-define("DIALOGFLOW_QUERY_ENDPOINT", "/df-query");
+// put client deployment url here, only used when IS_PRODUCTION = true
+// therefore, must start with 'https://'
+define("DEPLOYMENT_URL", "https://www.pieceofcode.org");
+
+// set port number, only used when IS_PRODUCTION = false
+define("LOCALHOST_PORT", "3000");
+
+// golang server url, only used when DIRECT_QUERY = false
+define("SERVER_URL", "https://www.pieceofcode.org:8080"); // when IS_PRODUCTION = true
+define("SERVER_URL_LOCAL", "http://localhost:8080"); // when IS_PRODUCTION = false
+
+
+// -------------------- STATIC --------------------
+// this section is mostly static, there is no need to change them for most of the time
+
+define("SERVER_ENDPOINT", "/internal-query"); // endpoint used when DIRECT_QUERY = false
+define("DIALOGFLOW_QUERY_ENDPOINT", "/df-query"); // endpoint used when DIRECT_QUERY = true
+define("PREPROCESS_ENDPOINT", "/preprocess"); // client-side spellchecking endpoint
+
+define("LOCALHOST", "http://localhost");
