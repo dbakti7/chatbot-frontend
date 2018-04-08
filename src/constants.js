5cd62ea4615be333dc1fd9453b0f7edbd31fd7cc
dbakti7 <dbakti1605@gmail.com>
2018-04-08 16:16:30 +0800

    Documentation.

--- src/constants.js
+++ src/constants.js
@@ -8,24 +8,20 @@ function define(name, value) {
 // set to true to deploy with HTTPS
 define("IS_PRODUCTION", false);
 
-// SSL Certs locations
+// SSL Certs locations, only used when IS_PRODUCTION is true
 define("CERT_FILE", "/etc/letsencrypt/live/www.pieceofcode.org/fullchain.pem");
 define("KEY_FILE", "/etc/letsencrypt/live/www.pieceofcode.org/privkey.pem");
 
-// port number for local deployment
-// if IS_PRODUCTION is true, 443 is used by default
 define("LOCALHOST", "http://localhost");
-define("LOCALHOST_PORT", "3000");
+define("LOCALHOST_PORT", "3000"); // only used when IS_PRODUCTION is false
 
-// server url and end-point as proxy to Dialogflow
-define("SERVER_URL", "https://www.pieceofcode.org:8080");
-define("SERVER_URL_LOCAL", "http://localhost:8080");
-define("SERVER_ENDPOINT", "/internal-query");
-
-// enable client-side spellchecking?
+// set to true to enable client-side spellchecking, false otherwise
 define("USE_SPELLCHECKING", true);
 
 // client-side spellchecking endpoint
 define("PREPROCESS_ENDPOINT", "/preprocess");
 
-
+// server url and end-point as proxy to Dialogflow
+define("SERVER_URL", "https://www.pieceofcode.org:8080");
+define("SERVER_URL_LOCAL", "http://localhost:8080");
+define("SERVER_ENDPOINT", "/internal-query");
